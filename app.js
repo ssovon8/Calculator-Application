ba40439f29190a14a4f55d0fe9247d93bad27a86
ssovon8 <ssovon8@gmail.com>
2026-03-02 16:45:48 +0530

    Create new Calculator Application

---
+++ app.js
@@ -0,0 +1,54 @@
+let form = document.querySelector("form");
+let display = document.querySelector("#display");
+let buttons = document.querySelector(".buttons");
+
+// Safe calculation function
+function safeCalculate(expression) {
+  try {
+    if (!/^[0-9+\-*/%.() ]+$/.test(expression)) {
+      throw new Error("Invalid Charecters");
+    }
+
+    let result = Function("return " + expression)();
+    if (!Number.isInteger(result)) {
+      result = result.toFixed(2);
+    }
+    return result;
+  } catch (error) {
+    return "Error";
+  }
+}
+
+// Handling function for mouse click event
+function handleInput(input) {
+  if (input === "=") {
+    display.value = safeCalculate(display.value);
+  } else if (input === "AC") {
+    display.value = "";
+  } else if (input === "C") {
+    display.value = display.value.slice(0, -1);
+  } else {
+    display.value += input;
+  }
+  display.focus();
+}
+
+// mouse click
+buttons.addEventListener("click", (evt) => {
+  if (evt.target.tagName === "INPUT") {
+    handleInput(evt.target.value);
+  }
+});
+
+// // keyboard support
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    display.value = safeCalculate(display.value);
+  } else if (e.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  } else if (e.key === "Escape") {
+    display.value = "";
+  }
+  display.focus();
+});
